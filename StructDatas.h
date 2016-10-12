e64c10a25f322abf0c8a98004fc0bd8798aaf286
Nico <nicolasfaraci@outlook.com>
2016-10-12 15:34:59 +0200

    Ajout de commentaire sur les structures communes

--- StructDatas.h
+++ StructDatas.h
@@ -1,7 +1,8 @@
 /*
- * Structures communes de la partie analyse syntaxique
+ * Structures de données communes
  */
 
+/* Enumérations */
 typedef enum {
     FUNCTION, REEL, VARIABLE, OPERATEUR, PARENT_OPEN, PARENT_CLOSE;
 } Lexeme;
@@ -18,10 +19,12 @@ typedef enum {
     101, 102, 103, 201, 202, 203, 301, 302, 303, 401, 402, 403;
 } CodeErreur;
 
+/* Gestion erreurs */
 typedef struct {
     CodeErreur codeErreur;
 } Erreur;
 
+/* Partie 1 : Analyse Lexicale*/
 typedef union {
     Fonction fonction;
     Operateur operateur;
@@ -33,9 +36,14 @@ typedef struct {
     Valeur valeur;
 } Jeton;
 
+/* Partie 2 : Analyse Syntaxique */
 typedef struct {
     Jeton jeton;
     Noeud* jeton_g;
     Noeud* jeton_d;
     Noeud* parent;
 } Noeud;
+
+/* Partie 3 : Evaluation */
+
+/* Partie 4 : Graphique */
