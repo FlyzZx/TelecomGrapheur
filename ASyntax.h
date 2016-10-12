c28941a965d485a5c6bd338ea6ec408c2fa60b7a
Nico <nicolasfaraci@outlook.com>
2016-10-12 15:01:21 +0200

    MAJ Struct analyse syntaxe
    Nico & Thomas

--- ASyntax.h
+++ ASyntax.h
@@ -2,6 +2,29 @@
  * Structures communes de la partie analyse syntaxique
  */
 
+typedef enum {
+    FUNCTION, REEL, VARIABLE, OPERATEUR, PARENT_OPEN, PARENT_CLOSE;
+} Lexeme;
+
+typedef enum {
+    PLUS, MOINS, MULT, DIV;
+} Operateur;
+
+typedef enum {
+    SIN, COS, TAN;
+} Fonction;
+
+typedef union {
+    Fonction fonction;
+    Operateur operateur;
+    float value;
+} Valeur;
+
+typedef struct {
+    Lexeme lexeme;
+    Valeur valeur;
+} Jeton;
+
 typedef struct {
     Jeton jeton;
     Noeud* jeton_g;
