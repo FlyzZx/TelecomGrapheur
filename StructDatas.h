964863dbaa09b285e57ed0bfa330ac11108a42c0
Nico <nicolasfaraci@outlook.com>
2016-10-12 15:18:52 +0200

    MAJ fichier structures commun et ajout des erreurs

--- StructDatas.h
+++ StructDatas.h
@@ -14,6 +14,14 @@ typedef enum {
     SIN, COS, TAN;
 } Fonction;
 
+typedef enum {
+    101, 102, 103, 201, 202, 203, 301, 302, 303, 401, 402, 403;
+} CodeErreur;
+
+typedef struct {
+    CodeErreur codeErreur;
+} Erreur;
+
 typedef union {
     Fonction fonction;
     Operateur operateur;
