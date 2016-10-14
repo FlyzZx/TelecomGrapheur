97100352ab6d777427cd5fb88d0366ec960aa78e
Nico <nicolasfaraci@outlook.com>
2016-10-14 08:12:42 +0200

    Modification du main.cpp

--- main.cpp
+++ main.cpp
@@ -1,25 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/* 
- * File:   main.cpp
- * Author: nicolas.faraci
- *
- * Created on 14 octobre 2016, 08:09
- */
-
 #include <cstdlib>
 
 using namespace std;
 
 /*
- * 
+ * Main de test pour la partie analyseur syntaxique
  */
 int main(int argc, char** argv) {
-
     return 0;
 }
 
