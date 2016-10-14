552cb21f771713c35f8a76f1a9722fbe950fa981
Nico <nicolasfaraci@outlook.com>
2016-10-14 08:11:14 +0200

    Ajout gitignore
    Ignore les dossiers netbeans build, dist et nbproject

---
+++ main.cpp
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: nicolas.faraci
+ *
+ * Created on 14 octobre 2016, 08:09
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
