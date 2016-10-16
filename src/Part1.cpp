1175a7aba5d80ac0297e416fb10ee70154175b2f
flotan11 <mardon.florian@gmail.com>
2016-10-16 17:47:17 +0200

    projet en c++ et hello world part1
    Convertion du projet en projet c++ et affichage de la partie 1 dans le
    dossier src (Part1.cpp) représenté par un helloworld autogénéré pour
    l'instant.

---
+++ src/Part1.cpp
@@ -0,0 +1,14 @@
+//============================================================================
+// Name        : Part1.cpp
+// Author      : Mardon Florian
+// Version     :
+// Copyright   : Copyright Mardon Florian & Co
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
