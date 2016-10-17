f5f75e52318828ed84a7c165325941793f0cd9d9
flotan11 <mardon.florian@gmail.com>
2016-10-17 11:42:27 +0200

    Avancé Part1
    Toutes les méthodes sont presques terminés, il reste encore quelques
    point à finaliser (TODO)

--- src/Part1.cpp
+++ src/Part1.cpp
@@ -16,45 +16,95 @@ int main() {
 }
 
 bool isFunction(char cara){
-	//TODO
+	if ((cara>'A' && cara<'Z' && cara!='X') || (cara>'a' && cara<'z' && cara!='x')){
+		return true;
+	}
+	return false;
 }
 
 bool isNumber(char cara){
-	//TODO
+	if (cara>='0' && cara<='9'){
+		return true;
+	}
+	return false;
 }
 
 bool isVar(char cara){
-	//TODO
+	if (cara=='x' || cara=='X'){
+		return true;
+	}
+	return false;
 }
 
 bool isOperator(char cara){
-	//TODO
+	if (cara=='+' || cara=='-' || cara=='*' || cara=='/'){
+		return true;
+	}
+	return false;
 }
 
 bool isParenthesisOpens(char cara){
-	//TODO
+	if(cara=='('){
+		return true;
+	}
+	return false;
 }
 
 bool isParenthesisClosing(char cara){
-	//TODO
+	if(cara==')'){
+		return true;
+	}
+	return false;
 }
 
-string toLexeme(char cara){
-	if (isFunction(cara)){
-	//TODO chercher comment verifier la fonction sur plusieurs caractères (sin,cos,... => plusieurs caractères)
-		return "FUNCTION";
-	}else if (isNumber(cara)){
-	//TODO chercher comment verifier la fonction sur plusieurs caractères (42,267,... => plusieurs caractères)
-		return "REEL";
-	}else if (isVar(cara)){
-		return "VARIABLE";
-	}else if (isOperator(cara)){
-		return "OPERATEUR";
-	}else if (isParenthesisOpens(cara)){
-		return "PARENT_OPEN";
-	}else if (isParenthesisClosing(cara)){
-		return "PARENT_CLOSE";
-	}else{
-	// TODO retourner une erreur référent à un caractère inconnus dans l'operation
+string* toLexeme(string chaine){
+	char cara;
+	int i=0;
+	while (i<chaine.length){
+		cara=chaine[i];
+		if (isFunction(cara)){
+			string chainePro=cara+"";
+			i++;
+			cara=chaine[i];
+			while (isFunction(cara)){
+				chainePro=chainePro+cara;
+				i++;
+				cara=chaine[i];
+			}
+			//TODO Verifier que la fonction existe bien
+			return "FUNCTION";
+		}else if (isNumber(cara)){
+			bool verif=false;
+			bool verif2=false;
+			while (isNumber(cara) || cara==',' || cara=='.'){
+				if (verif==false && (cara==',' || cara=='.')){
+					verif=true;
+					verif2=true;
+				}else if(verif==true && (cara==',' || cara=='.')){
+					//TODO retourner code erreur référent à 2 virgules présentes
+				}else{
+					verif2=false;
+				}
+				i++;
+				cara=chaine[i];
+			}
+			if (verif2==true){
+				//TODO retourner code erreur référent à virgule de fin
+			}else{
+				return "REEL";
+				i--;
+			}
+		}else if (isVar(cara)){
+			return "VARIABLE";
+		}else if (isOperator(cara)){
+			return "OPERATEUR";
+		}else if (isParenthesisOpens(cara)){
+			return "PARENT_OPEN";
+		}else if (isParenthesisClosing(cara)){
+			return "PARENT_CLOSE";
+		}else{
+		// TODO retourner une erreur référent à un caractère inconnus dans l'operation
+		}
+		i++;
 	}
 }
