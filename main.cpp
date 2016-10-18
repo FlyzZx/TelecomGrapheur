25e7eb0e1a6e497ff90677f1922c1922f636badd
Nico <nicolasfaraci@outlook.com>
2016-10-18 16:23:40 +0200

    Changement de fonction de test

--- main.cpp
+++ main.cpp
@@ -7,18 +7,22 @@ int main()
 {
     std::vector<Jeton> entree;
     Jeton j;
-    j.lexeme = FUNCTION;
-    j.valeur.fonction = SIN;
+    // 3 * (3+x) * 2
+    j.lexeme = REEL;
+    j.valeur.value = 3;
+    entree.push_back(j);
+    j.lexeme = OPERATEUR;
+    j.valeur.operateur = MULT;
     entree.push_back(j);
     j.lexeme = PARENT_OPEN;
     entree.push_back(j);
-    j.lexeme = VARIABLE;
+    j.lexeme = REEL;
+    j.valeur.value = 3;
     entree.push_back(j);
     j.lexeme = OPERATEUR;
     j.valeur.operateur = PLUS;
     entree.push_back(j);
-    j.lexeme = REEL;
-    j.valeur.value = 2;
+    j.lexeme = VARIABLE;
     entree.push_back(j);
     j.lexeme = PARENT_CLOSE;
     entree.push_back(j);
@@ -26,7 +30,7 @@ int main()
     j.valeur.operateur = MULT;
     entree.push_back(j);
     j.lexeme = REEL;
-    j.valeur.value = 6;
+    j.valeur.value = 2;
     entree.push_back(j);
 
     Noeud *arbre;
