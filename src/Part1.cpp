54c454c4d6d73c4ab5b73381c4a6d2e168f82a57
flotan11 <mardon.florian@gmail.com>
2016-10-27 15:05:53 +0200

    Methode aToken (Hadoken :) ) Finale
    Le code est complet au niveau des méthode. La méthode toLexeme a été
    changer par aToken car elle éfféctue mtn toute la mise en place des
    Jetons. Les résultats ne sont pas encore au point mais le code majeur
    est terminé, il ne reste plus que des modifs mineurs.

--- src/Part1.cpp
+++ src/Part1.cpp
@@ -7,11 +7,12 @@
 // TODO: Ajout du traitement des décimaux
 //============================================================================
 #include <iostream>
+#include <sstream>
 #include "../StructDatas.h"
 #include <string>
 #include <vector>
 using namespace std;
-string mainString="tan(2x+2)";
+string mainString="tan(22,25x+2)";
 
 bool isFunction(char cara){
 	if ((cara>='A' && cara<='Z' && cara!='X') || (cara>='a' && cara<='z' && cara!='x')){
@@ -55,13 +56,18 @@ bool isParenthesisClosing(char cara){
 	return false;
 }
 
-vector<string> toLexeme(string chaine){
-	vector<string> res;
+vector<Jeton> aToken(string chaine){ // a prononcer "haDoken" pour la blague ;)
+	vector<Jeton> res;
 	char cara;
+
+	Lexeme lex; // définis la correspondance de la valeur associé dans le token (jeton)
+	Valeur val; // définis par fonction OU opérateur OU valeur
+
 	int i=0;
 	while (i<=(int)sizeof(chaine)){
 		cara=chaine[i];
 		if (isFunction(cara)){
+			lex=FUNCTION;
 			string chainePro="";
 			chainePro=chainePro+cara;
 			i++;
@@ -72,21 +78,35 @@ vector<string> toLexeme(string chaine){
 				cara=chaine[i];
 			}
 			i--;
-			if ( chainePro.compare("sin") == 0 || chainePro.compare("SIN") == 0 || chainePro.compare("cos") == 0 || chainePro.compare("COS") == 0 || chainePro.compare("sqrt") == 0 || chainePro.compare("SQRT") == 0 || chainePro.compare("tan") == 0 || chainePro.compare("TAN") == 0 || chainePro.compare("log") == 0 || chainePro.compare("LOG") == 0 || chainePro.compare("ln") == 0 || chainePro.compare("LN") == 0) {
-					res.push_back("FUNCTION");
+			if ( chainePro.compare("sin") == 0 || chainePro.compare("SIN") == 0){
+				val.fonction=SIN;
+			}else if(chainePro.compare("cos") == 0 || chainePro.compare("COS") == 0 ){
+				val.fonction=COS;
+			}else if(chainePro.compare("sqrt") == 0 || chainePro.compare("SQRT") == 0){
+				val.fonction=SQRT;
+			}else if(chainePro.compare("tan") == 0 || chainePro.compare("TAN") == 0){
+				val.fonction=TAN;
+			}else if(chainePro.compare("log") == 0 || chainePro.compare("LOG") == 0){
+				val.fonction=LOG;
+			}else if(chainePro.compare("ln") == 0 || chainePro.compare("LN") == 0){
+				val.fonction=LN;
 			}else{
 				//TODO retourner code erreur référent à fonction non reconnus
 			}
 		}else if (isNumber(cara)){
+			lex=REEL;
 			bool verif=false;
 			bool verif2=false;
+			string nomb="";
 			while (isNumber(cara) || cara==',' || cara=='.'){
 				if (verif==false && (cara==',' || cara=='.')){
 					verif=true;
 					verif2=true;
+					nomb=nomb+cara;
 				}else if(verif==true && (cara==',' || cara=='.')){
 					//TODO retourner code erreur référent à 2 virgules présentes
 				}else{
+					nomb=nomb+cara;
 					verif2=false;
 				}
 				i++;
@@ -95,29 +115,47 @@ vector<string> toLexeme(string chaine){
 			if (verif2==true){
 				//TODO retourner code erreur référent à virgule de fin
 			}else{
-				res.push_back("REEL");
+				stringstream ss(nomb);
+				float f=0;
+				ss >> f;
+				val.value=f;
 				i--;
 			}
 		}else if (isVar(cara)){
-			res.push_back("VARIABLE");
+			lex=VARIABLE;
 		}else if (isOperator(cara)){
-			res.push_back("OPERATEUR");
+			lex=OPERATEUR;
+			if (cara=='+'){
+				val.operateur=PLUS;
+			}else if(cara=='-'){
+				val.operateur=MOINS;
+			}else if(cara=='*'){
+				val.operateur=MULT;
+			}else if(cara=='/'){
+				val.operateur=DIV;
+			}else{
+			// TODO fatal error impossible a atteindre normallement
+			}
 		}else if (isParenthesisOpens(cara)){
-			res.push_back("PARENT_OPEN");
+			lex=PARENT_OPEN;
 		}else if (isParenthesisClosing(cara)){
-			res.push_back("PARENT_CLOSE");
+			lex=PARENT_CLOSE;
 		}else{
 		// TODO retourner une erreur référent à un caractère inconnus dans l'operation
 		}
+		Jeton token;
+		token.lexeme=lex;
+		token.valeur=val;
+		res.push_back(token);
 		i++;
 	}
 	return res;
 }
 
 int main(){
-	vector<string> resultat=toLexeme(mainString);
+	vector<Jeton> resultat=aToken(mainString);
 	for (int i=0;i<resultat.size();i++){
-		cout << (resultat.at(i));
+		cout << (resultat.at(i).lexeme);
 	}
 	return 0;
 }
